ff2e7c97a9951e2e52cc1fce01a84948f770e7fb
inashinya <inas9730@gmail.com>
2019-07-29 16:03:42 +0900

    gridview

---
+++ 01-RecyclerView/app/src/main/java/com/andresjakl/inasamplerecyclerview/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.andresjakl.inasamplerecyclerview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.GridView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var titleText: TextView? = null
+    private var prevButton: Button? = null
+    private var nextButton: Button? = null
+    private var mCalendarAdapter: CalendarAdapter? = null
+    private var calendarGridView: GridView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        titleText = findViewById(R.id.titleText)
+        prevButton = findViewById(R.id.prevButton)
+        prevButton!!.setOnClickListener {
+            mCalendarAdapter!!.prevMonth()
+            titleText!!.text = mCalendarAdapter!!.title
+        }
+        nextButton = findViewById(R.id.nextButton)
+        nextButton!!.setOnClickListener {
+            mCalendarAdapter!!.nextMonth()
+            titleText!!.text = mCalendarAdapter!!.title
+        }
+        calendarGridView = findViewById(R.id.calendarGridView)
+        mCalendarAdapter = CalendarAdapter(this)
+        calendarGridView!!.adapter = mCalendarAdapter
+        titleText!!.text = mCalendarAdapter!!.title
+    }
+
+}
